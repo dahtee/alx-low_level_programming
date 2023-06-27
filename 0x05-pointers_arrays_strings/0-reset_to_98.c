0fd34070fc982a1107f665b36da6424f6f289856
dahtee <datibosa@gmail.com>
2023-06-27 09:51:25 -0700

    /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/9/../../../x86_64-linux-gnu/Scrt1.o: in function `_start: (.text+0x24): undefined reference to `main

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- *reset_to_98 - reset value of n to 98
- *@n - pointer to interger value
- *This function takes a pointer to an integer 'n' and modifies the value
- *Return: n
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 void reset_to_98(int *n);
